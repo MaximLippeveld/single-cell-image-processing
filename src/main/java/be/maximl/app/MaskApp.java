405a67785be216a9a5af5ade9855ae3ce13ecae6
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-12-15 10:12:09 +0100

    Start masking app

---
+++ src/main/java/be/maximl/app/MaskApp.java
@@ -0,0 +1,8 @@
+package be.maximl.app;
+
+import org.scijava.command.Command;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, menuPath = "Plugins>Single-cell imaging>Mask")
+public class MaskApp {
+}
